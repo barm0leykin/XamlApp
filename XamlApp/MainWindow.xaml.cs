112dfb7cf94c30f2e708e8fba07fa3945b562a28
ak <ak@KAV-WS241150>
2016-04-19 10:41:08 +0300

    Добавьте файлы проекта.

---
+++ XamlApp/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace XamlApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Button myButton = new Button();
+            //myButton.Width = 100;
+            //myButton.Height = 30;
+            //myButton.Content = "Кнопка";
+            //layoutGrid.Children.Add(myButton);
+        }
+    }
+}
+
